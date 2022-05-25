37c8fa907aed7fdf99bca8457e2b6dc4566b7995
guidipolito <guilhermepereiradipolito@hotmail.com>
2022-05-25 20:58:04 -0300

    feat(events): Adicionado visual dos botões em meus eventos

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -29,12 +29,18 @@
                     <p>{{$event->usersJoined()->count()}} pessoas planejam participar</p>
                     <p>Data de acontecimendo: {{ date('d/m/y', strtotime($event->date)) }}</p>
                 </div>
-                <a class="edit-btn" href="events/edit/{{$event->id}}">Editar</a>
-                <form method="POST" action="/events/{{$event->id}}">
-                    @csrf
-                    @method('DELETE')
-                    <button class="remove-btn">Remover</button>
-                </form>
+                <div class="event_actions">
+                    <a class="edit-btn" href="events/edit/{{$event->id}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
+                    </a>
+                    <form method="POST" action="/events/{{$event->id}}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="remove-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-delete"><path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zM18 9l-6 6M12 9l6 6"/></svg>
+                        </button>
+                    </form>
+                </div>
             </li>
         @endforeach
         </ul>
